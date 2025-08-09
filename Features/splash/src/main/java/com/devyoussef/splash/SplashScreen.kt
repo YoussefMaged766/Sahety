ae79f92f06b66a5f672b7e694dbafd748c510a01
YoussefMaged766 <yoer766@gmail.com>
2025-08-09 19:16:39 +0300

    Add splash screen colors and update theme for improved UI update splash background based on theme on api 31 and above

--- Features/splash/src/main/java/com/devyoussef/splash/SplashScreen.kt
+++ Features/splash/src/main/java/com/devyoussef/splash/SplashScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -47,7 +48,7 @@ fun SplashScreen(
     val textProgress = remember { Animatable(0f) }
     val textAlpha = remember { Animatable(0f) }
 
-    val appTitle = "Sehaty"
+    val appTitle = stringResource(com.devyoussef.splash.R.string.sehaty)
 
     // Build animated text
     val animatedText = buildAnnotatedString {
