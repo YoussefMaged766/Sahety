0d23f24fe470008859ae176328548ee96b146cd0
YoussefMaged766 <yoer766@gmail.com>
2025-08-12 22:04:15 +0300

    Update JDK version from 11 to 17 in build.gradle.kts

--- Features/splash/build.gradle.kts
+++ Features/splash/build.gradle.kts
@@ -31,13 +31,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     tasks.withType<KotlinJvmCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
             freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
+
         }
     }
 }
